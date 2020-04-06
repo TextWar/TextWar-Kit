d59c3598cbef32936c33fe44ebafd99edf27b1c2
magiclu550 <843983728@qq.com>
2020-04-06 23:00:01 +0800

    download

--- download_env/get_source.sh
+++ download_env/get_source.sh
@@ -20,6 +20,7 @@ wget https://github.com/TextWar/TextWar/archive/master.zip
 unzip master.zip
 rm -f master.zip
 cd master
+pwd
 mvn clean compile package install -f textwar_pom/pom.xml
 mvn clean compile package install -f textwar/pom.xml
 mvn clean compile package install -f textwar_protocol/pom.xml
