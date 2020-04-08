d6880c32dfea1d760d4e968beb00ed6e7ae2f3b8
magiclu550 <843983728@qq.com>
2020-04-08 09:01:53 +0800

    download py

--- download_env/get_source.sh
+++ download_env/get_source.sh
@@ -14,12 +14,10 @@ if [ $? -ne 0 ]; then
     cd ..
     rm -f apache-maven-3.6.3-bin.zip
 fi
-mkdir textwar_py
-cd textwar_py
 wget https://github.com//TextWar/TextWar-PythonPlugin/archive/master.zip
 unzip master.zip
+mv TextWar-PythonPlugin-master textwar_py
 rm -rf master.zip
-cd ..
 wget https://github.com/TextWar/TextWar/archive/master.zip
 unzip master.zip
 rm -f master.zip
